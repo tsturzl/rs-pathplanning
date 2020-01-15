eec9b4d717c8ea05a17154f79762c14149815073
Travis Sturzl <travissturzl@gmail.com>
2020-01-15 00:29:07 -0700

    random node generation

--- src/rrt.rs
+++ src/rrt.rs
@@ -1,6 +1,8 @@
-use geo::{LineString, Polygon, Rect, Coordinate};
+use geo::{LineString, Polygon, Rect, Coordinate, Point};
 use geo::algorithm::{intersects::Intersects, contains::Contains};
 use std::f64::consts::PI;
+use std::rand::{task_rng, Rng};
+use std::cmp;
 
 pub struct Robot {
     width: f64,
@@ -65,7 +67,7 @@ impl Space {
         let last_point = line_points[line_points.len()];
         let robot_poly = self.robot.create_poly(last_point.into());
 
-        if (self.bounds.contains(line) && self.bounds.contains(&robot_poly)) == false {
+        if !(self.bounds.contains(line) && self.bounds.contains(&robot_poly)) {
             false
         } else {
             let intersected = self.obstacles.iter()
@@ -74,35 +76,84 @@ impl Space {
             !intersected
         }
     }
+
+    pub fn rand_point(&self) -> Point<f64> {
+        let points = self.bounds.exterior().into_points();
+        let (fx, fy) = points[0].x_y();
+        let mut minx = fx;
+        let mut maxx = fx;
+        let mut miny = fy;
+        let mut maxy = fy;
+
+        for point in points {
+            let (x, y) = point.x_y();
+            if x < minx {
+                minx = x;
+            }
+            if x > maxx {
+                maxx = x;
+            }
+
+            if y < miny {
+                miny = y;
+            }
+            if y > maxy {
+                maxy = y;
+            }
+        }
+
+        let randx: f64 = task_rng().gen_range(minx, maxx);
+        let randy: f64 = task_rng().gen_range(miny, maxy);
+
+        Point::new(randx, randy)
+    }
 }
 
 pub struct Node<'b> {
     coord: Coordinate<f64>,
-    heading: f64,
     children: Vec<&'b Node<'b>>,
 } 
 
+impl<'b> Node<'b> {
+    pub fn add_child(&self, node: &'b Node) {
+        self.children.push(node);
+    }
+}
+
 pub trait DrawLine {
-    fn new(node: &Node) -> Self;
-    fn draw(&self) -> &LineString<f64>;
+    fn draw(&self, node: &Node) -> &LineString<f64>;
 }
 
 pub struct RRT<'a, T: DrawLine> {
-    start: (Coordinate<f64>, f64),
-    goal: (Coordinate<f64>, f64),
+    child_limit: usize,
+    start: Coordinate<f64>,
+    goal: Coordinate<f64>,
     space: Space,
     root: Node<'a>,
     line_type: T,
 }
 
 impl<'a, T: DrawLine> RRT<'a, T> {
-    pub fn new(&self, start: (Coordinate<f64>, f64), goal: (Coordinate<f64>, f64), space: Space, line_type: T) -> RRT<'a, T> {
+    pub fn new(start: Coordinate<f64>, goal: Coordinate<f64>, space: Space, line_type: T) -> RRT<'a, T> {
         RRT::<'a, T> {
+            child_limit: 100,
             start,
             goal,
             space,
             line_type,
-            root: Node::<'a> { coord: start.0, heading: start.1, children: vec![]},
+            root: Node::<'a> { coord: start, children: vec![]},
         }
+    }
+
+    pub fn verify_node(&self, node: &Node) -> bool {
+        let line = self.line_type.draw(node);
+        self.space.verify(&line)
+    }
+
+    pub fn create_node(&self) -> &Node {
+        let coord = self.space.rand_point().into();
+        let node = Node { coord, children: vec![]};
+
+        &node
     }
 }
