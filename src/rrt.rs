1da7a09773fe7d4f653cc6eb4d839e24dcfad6f1
tsturzl <tsturzl@lefthandrobotics.com>
2020-01-17 17:39:55 -0700

    trying boxes for nodes

--- src/rrt.rs
+++ src/rrt.rs
@@ -50,33 +50,15 @@ pub struct Space {
     bounds: Polygon<f64>,
     robot: Robot,
     obstacles: Vec<Polygon<f64>>,
+    minx: f64,
+    maxx: f64,
+    miny: f64,
+    maxy: f64,
 }
 
 impl Space {
     pub fn new(bounds: Polygon<f64>, robot: Robot, obstacles: Vec<Polygon<f64>>) -> Space {
-        Space {
-            bounds,
-            robot,
-            obstacles,
-        }
-    }
-
-    pub fn verify(&self, line: &LineString<f64>) -> bool {
-        let last_point = line.points_iter().last().expect("Linestring should have a last point");
-        let robot_poly = self.robot.create_poly(last_point);
-
-        if !(self.bounds.contains(line) && self.bounds.contains(&robot_poly)) {
-            false
-        } else {
-            let intersected = self.obstacles.iter()
-                .map(|obs| (line.intersects(obs) && robot_poly.intersects(obs)))
-                .fold(false, |acc, x| acc && x);
-            !intersected
-        }
-    }
-
-    pub fn rand_point(&self) -> Point<f64> {
-        let ext = self.bounds.exterior();
+        let ext = bounds.exterior();
         let points: Vec<Point<f64>> = ext.points_iter().collect();
         let (fx, fy) = points[0].x_y();
         let mut minx = fx;
@@ -101,23 +83,49 @@ impl Space {
             }
         }
 
+        Space {
+            bounds,
+            robot,
+            obstacles,
+            minx,
+            maxx,
+            miny,
+            maxy,
+        }
+    }
+
+    pub fn verify(&self, line: &LineString<f64>) -> bool {
+        let last_point = line.points_iter().last().expect("Linestring should have a last point");
+        let robot_poly = self.robot.create_poly(last_point);
+
+        if !(self.bounds.contains(line) && self.bounds.contains(&robot_poly)) {
+            false
+        } else {
+            let intersected = self.obstacles.iter()
+                .map(|obs| (line.intersects(obs) && robot_poly.intersects(obs)))
+                .fold(false, |acc, x| acc && x);
+            !intersected
+        }
+    }
+
+    pub fn rand_point(&self) -> Point<f64> {
         let mut rng = thread_rng();
 
-        let randx: f64 = rng.gen_range(minx, maxx);
-        let randy: f64 = rng.gen_range(miny, maxy);
+        let randx: f64 = rng.gen_range(self.minx, self.maxx);
+        let randy: f64 = rng.gen_range(self.miny, self.maxy);
 
         Point::new(randx, randy)
     }
 }
 
-pub struct Node<'b> {
+pub struct Node {
     point: Point<f64>,
-    children: Vec<&'b Node<'b>>,
+    children: Vec<Box<Node>>,
     cost: f64,
 } 
 
-impl<'b> Node<'b> {
-    pub fn add_child(&mut self, node: &'b Node<'b>) {
+impl Node {
+    pub fn add_child(&mut self, node: Box<Node>) {
         self.children.push(node);
     }
 
@@ -139,7 +147,7 @@ impl<'b> Node<'b> {
     }
 }
 
-pub fn create_line(from: &Node, to: &Node) -> LineString<f64> {
+pub fn create_line(from: Box<Node>, to: Box<Node>) -> LineString<f64> {
     LineString(vec![
         from.get_coord(),
         to.get_coord(),
@@ -148,21 +156,23 @@ pub fn create_line(from: &Node, to: &Node) -> LineString<f64> {
 
 
 #[allow(dead_code)]
-pub struct RRT<'a> {
+pub struct RRT {
     start: Coordinate<f64>,
     goal: Coordinate<f64>,
     max_iter: usize,
+    goal_sample_rate: usize,
     space: Space,
-    nodes: Vec<Node<'a>>, // note: root node is the first item in this vector
+    nodes: Vec<Box<Node>>, // note: root node is the first item in this vector
 }
 
-impl<'a> RRT<'a> {
-    pub fn new(start: Coordinate<f64>, goal: Coordinate<f64>, max_iter: usize, space: Space) -> RRT<'a> {
-        let root = Node { point: start.into(), children: vec![], cost: 0.0};
+impl RRT {
+    pub fn new(start: Coordinate<f64>, goal: Coordinate<f64>, max_iter: usize, space: Space) -> RRT {
+        let root = Box::new(Node { point: start.into(), children: vec![], cost: 0.0});
         let rrt = RRT {
             start,
             goal,
             max_iter,
+            goal_sample_rate: 5,
             space,
             nodes: vec![root],
         };
@@ -172,9 +182,9 @@ impl<'a> RRT<'a> {
     
     // The new_node shouldn't be added to the RRT's `nodes` vec yet
     // a KD-Tree could speed this up
-    pub fn get_nearest_node(&self, new_node: &Node) -> &Node {
+    pub fn get_nearest_node(&mut self, new_node: Box<Node>) -> Box<Node> {
         let new_point = new_node.get_point();
-        let result: (&Node, f64) = self.nodes.iter()
+        let result: (Box<Node>, f64) = self.nodes.into_iter()
             .map(|node| {
                 let p = node.get_point();
                 (node, new_point.euclidean_distance(p))
@@ -184,13 +194,38 @@ impl<'a> RRT<'a> {
         result.0
     }
 
-    pub fn create_node(&self) -> Node {
+    pub fn get_random_node(&self) -> Box<Node> {
+
+        let mut rng = thread_rng();
+
+        if rng.gen_range(0, 100) > self.goal_sample_rate {
+
         let point = self.space.rand_point();
         let node = Node { point, children: vec![], cost: 0.0};
 
-        node
+        Box::new(node)
+        } else {
+            Box::new(Node { point: self.goal.into(), children: vec![], cost: 0.0})
+        }
     }
 
-    pub fn expand(&self) {
+    pub fn verify_node(&self, from_node: Box<Node>, to_node: Box<Node>) -> bool {
+        let line = create_line(from_node, to_node);
+        self.space.verify(&line)
+    }
+
+    #[allow(unused)]
+    pub fn plan(&mut self) {
+        for i in 0..self.max_iter {
+            let rnd_node = self.get_random_node();
+            let nearest_node = self.get_nearest_node(rnd_node);
+
+            if self.verify_node(nearest_node, rnd_node) {
+                self.nodes.push(rnd_node);
+                let new_node = self.nodes[self.nodes.len()];
+
+                nearest_node.add_child(new_node);
+            }
+        }
     }
 }
