8f91c88c0ded4f5c1a49f5ac51a3a33b0b356433
Travis Sturzl <travissturzl@gmail.com>
2020-01-19 12:36:36 -0700

    pass around index instead of reference

--- src/rrt.rs
+++ src/rrt.rs
@@ -1,7 +1,6 @@
 use geo::{LineString, Polygon, Rect, Coordinate, Point};
 use geo::algorithm::{intersects::Intersects, contains::Contains, euclidean_distance::EuclideanDistance};
 use std::f64::consts::PI;
-use std::rc::Rc;
 use rand::{thread_rng, Rng};
 
 pub struct Robot {
@@ -114,13 +113,18 @@ impl Space {
 }
 
 pub struct Node {
+    index: usize,
     point: Point<f64>,
-    parent: Option<Rc<Node>>,
+    parent: Option<usize>,
 } 
 
 impl Node {
-    pub fn get_parent(&self) -> Option<Rc<Node>> {
-        self.parent.clone()
+    pub fn get_index(&self) -> usize {
+        self.index
+    }
+
+    pub fn get_parent(&self) -> Option<usize> {
+        self.parent
     }
 
     pub fn get_point(&self) -> &Point<f64> {
@@ -132,7 +136,7 @@ impl Node {
     }
 }
 
-pub fn create_line(from: Rc<Node>, to: Rc<Node>) -> LineString<f64> {
+pub fn create_line(from: &Node, to: &Node) -> LineString<f64> {
     LineString(vec![
         from.get_coord(),
         to.get_coord(),
@@ -144,12 +148,12 @@ pub struct RRT {
     goal: Coordinate<f64>,
     max_iter: usize,
     space: Space,
-    nodes: Vec<Rc<Node>>, // note: root node is the first item in this vector
+    nodes: Vec<Node>, // note: root node is the first item in this vector
 }
 
 impl RRT {
     pub fn new(start: Coordinate<f64>, goal: Coordinate<f64>, max_iter: usize, space: Space) -> RRT {
-        let root = Rc::new(Node { point: start.into(), parent: None});
+        let root = Node { point: start.into(), index: 0, parent: None};
         RRT {
             goal,
             max_iter,
@@ -159,52 +163,57 @@ impl RRT {
     }
     
     // a KD-Tree could speed this up
-    pub fn get_nearest_node(&self, point: &Point<f64>) -> Rc<Node> {
-        let result: (Rc<Node>, f64) = self.nodes.iter()
+    pub fn get_nearest_node(&self, point: &Point<f64>) -> usize {
+        let result: (usize, f64) = self.nodes.iter()
             .map(|node| {
-                (node.clone(), point.euclidean_distance(node.get_point()))
+                (node.get_index(), point.euclidean_distance(node.get_point()))
             })
             .min_by(|a, b| a.1.partial_cmp(&b.1).expect("Node lengths(<f64>) should compare."))
             .expect("Should get the closest node");
-        result.0.clone()
+        result.0
     }
 
-    pub fn get_random_node(&self) -> Rc<Node> {
+    pub fn get_random_node(&mut self) -> usize {
         let point = self.space.rand_point();
         let nearest_node = self.get_nearest_node(&point);
 
-        Rc::new(Node { point, parent: Some(nearest_node.clone())})
+        self.nodes.push(Node { point, index: self.nodes.len(), parent: Some(nearest_node)});
+        self.nodes.len()
     }
 
-    pub fn verify_node(&self, node: Rc<Node>) -> bool {
-        match node.clone().get_parent() {
+    pub fn verify_node(&self, node: &Node) -> bool {
+        match node.get_parent() {
             Some(parent) => {
-                let line = create_line(parent, node.clone());
+                let line = create_line(&self.nodes[parent], node);
                 self.space.verify(&line)
             },
             None => false,
         }
     }
 
-    pub fn check_finish(&self, node: Rc<Node>) -> Option<Rc<Node>> {
-        let goal_node = Rc::new(Node { point: self.goal.into(), parent: Some(node.clone())});
+    pub fn check_finish(&mut self, _node: usize) -> Option<usize> {
+        let node = &self.nodes[_node];
+        self.nodes.push(Node { point: self.goal.into(), index: self.nodes.len(), parent: Some(node.get_index())});
+        let goal_node = &self.nodes[self.nodes.len()];
 
-        if self.verify_node(goal_node.clone()) {
-            Some(goal_node.clone())
+        if self.verify_node(goal_node) {
+            Some(goal_node.get_index())
         } else {
             None
         }
     }
     
-    pub fn finalize(&self, goal_node: Rc<Node>) -> LineString<f64> {
+    pub fn finalize(&self, _goal_node: usize) -> LineString<f64> {
+        let goal_node = &self.nodes[_goal_node];
         let mut points = vec![];
-        points.push(goal_node.clone().get_coord());
+        points.push(goal_node.get_coord());
 
         let mut parent = goal_node.get_parent();
 
-        while let Some(next_parent) = parent.clone() {
-            points.push(next_parent.get_coord());
-            parent = next_parent.get_parent();
+        while let Some(next_parent) = parent {
+            let n = &self.nodes[next_parent];
+            points.push(n.get_coord());
+            parent = n.get_parent();
         }
 
         points.reverse();
@@ -215,13 +224,15 @@ impl RRT {
     pub fn plan(&mut self) -> Option<LineString<f64>> {
         for _i in 0..self.max_iter {
             let rnd_node = self.get_random_node();
+            let node = &self.nodes[rnd_node];
 
-            if self.verify_node(rnd_node.clone()) {
-                self.nodes.push(rnd_node.clone());
+            if self.verify_node(node) {
 
-                if let Some(goal_node) = self.check_finish(rnd_node.clone()) {
-                    return Some(self.finalize(goal_node.clone()))
+                if let Some(goal_node) = self.check_finish(node.get_index()) {
+                    return Some(self.finalize(goal_node))
                 }
+            } else {
+                self.nodes.remove(rnd_node);
             }
         }
 
