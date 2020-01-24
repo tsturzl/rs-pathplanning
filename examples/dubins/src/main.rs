51b34acfbdde982bf1b6c663dff50c6bec74260e
tsturzl <tsturzl@lefthandrobotics.com>
2020-01-24 14:10:27 -0700

    not looking great

--- examples/dubins/src/main.rs
+++ examples/dubins/src/main.rs
@@ -2,7 +2,7 @@ extern crate gnuplot;
 extern crate pathplanning;
 
 use gnuplot::{Color, Figure};
-use pathplanning::dubins::dubins_path_linestring;
+use pathplanning::dubins::{dubins_path_linestring, DubinsConfig};
 
 fn main() {
     let mut fg = Figure::new();
@@ -22,19 +22,35 @@ fn main() {
     axes.points(vec![start_x], vec![start_y], &[Color("blue")])
         .points(vec![end_x], vec![end_y], &[Color("blue")]);
 
+    let conf1 = DubinsConfig {
+        sx: start_x,
+        sy: start_y,
+        syaw: start_yaw,
+        ex: end_x,
+        ey: end_y,
+        eyaw: end_yaw,
+        c: curvature,
+    };
+
+    let conf2 = DubinsConfig {
+        sx: end_x,
+        sy: end_y,
+        syaw: end_yaw,
+        ex: start_x,
+        ey: start_y,
+        eyaw: start_yaw,
+        c: curvature,
+    };
+
     println!("Start planner");
-    match dubins_path_linestring(
-        start_x, start_y, start_yaw, end_x, end_y, end_yaw, curvature,
-    ) {
+    match dubins_path_linestring(&conf1) {
         Some(line) => {
             let (px, py): (Vec<f64>, Vec<f64>) = line.points_iter().map(|p| p.x_y()).unzip();
             axes.lines(&px, &py, &[Color("red")]);
         }
         None => println!("Could not generate path"),
     }
-    match dubins_path_linestring(
-        end_x, end_y, end_yaw, start_x, start_y, start_yaw, curvature,
-    ) {
+    match dubins_path_linestring(&conf2) {
         Some(line) => {
             let (px, py): (Vec<f64>, Vec<f64>) = line.points_iter().map(|p| p.x_y()).unzip();
             axes.lines(&px, &py, &[Color("red")]);
