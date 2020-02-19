67fc712783e5447225d98afbb819990bedc64bc5
tsturzl <tsturzl@lefthandrobotics.com>
2020-02-19 09:56:43 -0700

    feed example with json

--- examples/rrt/src/main.rs
+++ examples/rrt/src/main.rs
@@ -1,48 +1,47 @@
 extern crate gnuplot;
 extern crate pathplanning;
+extern crate serde;
 
 use geo::{Coordinate, LineString, Point, Polygon, Rect};
 use gnuplot::{Color, Figure};
 use pathplanning::rrt::{create_circle, Robot, Space, RRT};
+use serde::{Deserialize, Serialize};
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+
+#[derive(Serialize, Deserialize)]
+struct Conf {
+    bounds: Vec<(f64, f64)>,
+    obstacles: Vec<Vec<(f64, f64)>>,
+    path: Vec<(f64, f64)>,
+    start: (f64, f64, f64),
+    goal: (f64, f64, f64),
+}
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    let file_path = &args[1];
+    let mut file = File::open(file_path).expect("file should open");
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)
+        .expect("should read file");
+
+    let conf: Conf = serde_json::from_str(&contents).expect("should deserialize");
+
     let mut fg = Figure::new();
 
-    let obstacle_list = vec![
-        Rect::new(
-            Coordinate { x: 0.0, y: 3.0 },
-            Coordinate { x: 1.0, y: 11.0 },
-        )
-        .into(),
-        Rect::new(Coordinate { x: 0.0, y: 3.0 }, Coordinate { x: 9.0, y: 4.8 }).into(),
-        Rect::new(
-            Coordinate { x: 8.0, y: 3.0 },
-            Coordinate { x: 11.0, y: 11.0 },
-        )
-        .into(),
-        // create_circle(Point::new(5.0, 5.0), 1.0),
-        // create_circle(Point::new(3.0, 6.0), 2.0),
-        // create_circle(Point::new(3.0, 8.0), 2.0),
-        // create_circle(Point::new(3.0, 10.0), 2.0),
-        // create_circle(Point::new(7.0, 5.0), 2.0),
-        // create_circle(Point::new(9.0, 5.0), 2.0),
-        // create_circle(Point::new(80.0, 100.0), 10.0),
-    ];
+    let obstacle_list: Vec<Polygon<f64>> = conf
+        .obstacles
+        .into_iter()
+        .map(|o| Polygon::new(LineString::from(o), vec![]))
+        .collect();
 
-    let bounds = Polygon::new(
-        LineString::from(vec![
-            (-6.0, -6.0),
-            (-6.0, 15.0),
-            (15.0, 15.0),
-            (15.0, -6.0),
-            (-6.0, -6.0),
-        ]),
-        vec![],
-    );
+    let bounds = Polygon::new(LineString::from(conf.bounds), vec![]);
 
     let (bx, by): (Vec<f64>, Vec<f64>) = bounds.exterior().points_iter().map(|p| p.x_y()).unzip();
 
-    let robot = Robot::new(1.0, 1.0, 0.8);
+    let robot = Robot::new(1.8, 3.0, 0.8);
     let space = Space::new(bounds, robot, obstacle_list.clone());
 
     let buffer_obs = space.get_obs();
@@ -54,10 +53,10 @@ fn main() {
         .unzip();
 
     let planner = RRT::new(
-        (5.0, 0.0).into(),
-        (-45.0_f64).to_radians(),
-        (6.0, 10.0).into(),
-        45.0_f64.to_radians(),
+        (conf.start.0, conf.start.1).into(),
+        conf.start.2,
+        (conf.goal.0, conf.goal.1).into(),
+        conf.goal.2,
         8000,
         0.1,
         space,
@@ -65,7 +64,12 @@ fn main() {
 
     let axes = fg.axes2d();
     axes.lines(&bx, &by, &[Color("black")])
-        .lines(&bbx, &bby, &[Color("blue")]);
+        .lines(&bbx, &bby, &[Color("blue")])
+        .points(
+            &[conf.start.0, conf.goal.0],
+            &[conf.start.1, conf.goal.1],
+            &[Color("green")],
+        );
 
     println!("Start planner");
     match planner.plan() {
